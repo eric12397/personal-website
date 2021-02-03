6009f683a6095967680906bb88d7a8e2c58f9b9a
Eric Choi <eric12397@gmail.com>
2021-02-02 21:41:54 -0500

    created landing page

---
+++ main.js
@@ -0,0 +1,10 @@
+gsap.from(".logo", {duration: 1, opacity: 0, y: 10})
+gsap.from(".hamburger", {duration: 1, opacity: 0, y: 50, stagger:0.2});
+gsap.from(".nav-link", {duration: 1, opacity: 0, y: 50, stagger:0.2});
+
+gsap.from(".landing-page-text h1", {duration: 1, opacity: 0, y: 10, delay: 0.2});
+gsap.from(".landing-page-text h2", {duration: 1, opacity: 0, y: 10, delay: 0.3});
+gsap.from(".landing-page-text p", {duration: 1, opacity: 0, y: 10, delay: 0.4});
+gsap.from(".landing-page-text .btn", {duration: 1, opacity: 0, y: 10, delay: 0.5});
+gsap.from(".landing-page-img", {duration: 1, opacity: 0, y: 25, delay: 0.5});
+
