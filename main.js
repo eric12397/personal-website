7f645dbc00340a8f00b49028259cc2c517d86c1b
Eric Choi <eric12397@gmail.com>
2021-02-07 23:10:37 -0500

    edited styles and added animations

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ gsap.from(".logo", {duration: 1, opacity: 0, y: 10})
 gsap.from(".hamburger", {duration: 1, opacity: 0, y: 50, stagger:0.2});
 gsap.from(".nav-link", {duration: 1, opacity: 0, y: 50, stagger:0.2});
 
-gsap.from(".landing-page-text h1", {duration: 1, opacity: 0, y: 10, delay: 0.2});
-gsap.from(".landing-page-text h2", {duration: 1, opacity: 0, y: 10, delay: 0.3});
-gsap.from(".landing-page-text p", {duration: 1, opacity: 0, y: 10, delay: 0.4});
-gsap.from(".landing-page-text .btn", {duration: 1, opacity: 0, y: 10, delay: 0.5});
-gsap.from(".landing-page-img", {duration: 1, opacity: 0, y: 25, delay: 0.5});
+gsap.from(".landing-page-text h1", {duration: 1, opacity: 0, y: 45, delay: 0.2});
+gsap.from(".landing-page-text h2", {duration: 1, opacity: 0, y: 45, delay: 0.3});
+gsap.from(".landing-page-text p", {duration: 1, opacity: 0, y: 45, delay: 0.4});
+gsap.from(".landing-page-text a", {duration: 1, opacity: 0, y: 45, delay: 0.5});
+
+gsap.from("#ring-1", {duration: 1, opacity: 0, y: 45, delay: 0.5});
+gsap.from("#ring-2", {duration: 1, opacity: 0, y: 65, delay: 0.6});
+gsap.from("#ring-3", {duration: 1, opacity: 0, y: 65, delay: 0.7});
+gsap.from("#ring-4", {duration: 1, opacity: 0, y: 65, delay: 0.8});
+gsap.from("#ring-5", {duration: 1, opacity: 0, y: 75, delay: 0.9});
+
+gsap.from("#molecule-1", {duration: 1, opacity: 0, y: 65, delay: 0.5});
+gsap.from("#molecule-2", {duration: 1, opacity: 0, y: 65, delay: 0.7});
+gsap.from("#molecule-3", {duration: 1, opacity: 0, y: 65, delay: 0.9});
 
