12c1faf238ea013b79bead06f5232e52d67abbaf
Eric Choi <eric12397@gmail.com>
2021-02-25 19:20:19 -0500

    updated landing page styles

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ gsap.from(".nav-link", {duration: 1, opacity: 0, y: 50, stagger:0.2});
 gsap.from(".landing-page-text h1", {duration: 1, opacity: 0, y: 45, delay: 0.2});
 gsap.from(".landing-page-text h2", {duration: 1, opacity: 0, y: 45, delay: 0.3});
 gsap.from(".landing-page-text p", {duration: 1, opacity: 0, y: 45, delay: 0.4});
-gsap.from(".landing-page-text a", {duration: 1, opacity: 0, y: 45, delay: 0.5});
+gsap.from(".landing-page-text .btn", {duration: 1, opacity: 0, y: 45, delay: 0.5});
 
 gsap.from("#ring-1", {duration: 1, opacity: 0, y: 45, delay: 0.5});
 gsap.from("#ring-2", {duration: 1, opacity: 0, y: 65, delay: 0.6});
