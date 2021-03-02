d8adfa5fee61bfb258afd6428c5fbdfdf7e3f848
Eric Choi <eric12397@gmail.com>
2021-03-01 19:47:19 -0500

    added gsap animations for portfolio section

--- animations.js
+++ animations.js
@@ -1,3 +1,5 @@
+// landing page animations
+
 gsap.from(".logo", {duration: 1, opacity: 0, y: 10})
 gsap.from(".hamburger", {duration: 1, opacity: 0, y: 50, stagger:0.2});
 gsap.from(".nav-link", {duration: 1, opacity: 0, y: 50, stagger:0.2});
@@ -16,4 +18,33 @@ gsap.from("#ring-5", {duration: 1, opacity: 0, y: 75, delay: 0.9});
 gsap.from("#molecule-1", {duration: 1, opacity: 0, y: 65, delay: 0.5});
 gsap.from("#molecule-2", {duration: 1, opacity: 0, y: 65, delay: 0.7});
 gsap.from("#molecule-3", {duration: 1, opacity: 0, y: 65, delay: 0.9});
+
+// scroll triggers
+gsap.registerPlugin(ScrollTrigger);
+
+let images = gsap.utils.toArray('.p-img img');
+images.forEach(item => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: item,
+      start: "top 75%",
+    }
+  });
+
+  tl.from(item, { x: -300, opacity: 0, duration: 1 })
+})
+
+
+let descriptions = gsap.utils.toArray('.p-description');
+descriptions.forEach(item => {
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: item,
+      start: "top 85%",
+    }
+  });
+
+  tl.from(item, { x: 300, opacity: 0, duration: 1 })
+})
+
 
