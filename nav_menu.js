366d82df521d00f35c92640c0c5a6171723401fd
Eric Choi <eric12397@gmail.com>
2021-03-04 15:07:59 -0500

    fixed nav menu issue on mobile screens

--- nav_menu.js
+++ nav_menu.js
@@ -1,8 +1,18 @@
 const menuBtn = document.getElementById("active");
 const navWrapper = document.querySelector(".nav-wrapper");
+const navWrapper_ul = document.querySelector(".nav-wrapper ul");
+
+menuBtn.addEventListener('change', function () {
+  if (this.checked) {
+    navWrapper_ul.classList.remove("hide");
+  } else {
+    navWrapper_ul.classList.add("hide")
+  }
+})
 
 navWrapper.addEventListener('click', function () {
   if (menuBtn.checked) {
     menuBtn.checked = false;
+    navWrapper_ul.classList.add("hide")
   }
 })
